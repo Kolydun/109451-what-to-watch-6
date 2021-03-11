3ea802d35304032d773178f84ba190c3bf5cf9c7
Kolydun <58638786+Kolydun@users.noreply.github.com>
2021-03-11 19:59:05 +0200

    Добавил переключение фильмов при удежании мышки на карточке фильма на 1 секунду.
    Добавил переключение фильмов при удежании мышки на карточке фильма на 1 секунду.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
+import SmallPlayer from "../small-player/small-player";
 
 const MovieCard = (props) => {
 
-  const {generatedFilm, stateFunction} = props;
+  const {generatedFilm, trackActiveFilmId} = props;
+  const [isPreviewPlaying, setIsPreviewPlaying] = useState({status: false});
+  const cardRef = useRef();
+
+  useEffect(() => {
+    let timer;
+
+    cardRef.current.onmouseenter = () => {
+      timer = setTimeout(() => {
+        setIsPreviewPlaying((prevIsPreviewPlaying) => ({
+          ...prevIsPreviewPlaying,
+          status: true,
+        }));
+      }, 1000);
+    };
+
+    cardRef.current.onmouseleave = () => {
+      clearTimeout(timer);
+      setIsPreviewPlaying((prevIsPreviewPlaying) => ({
+        ...prevIsPreviewPlaying,
+        status: false,
+      }));
+    };
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <React.Fragment>
-      <article className="small-movie-card catalog__movies-card" onMouseEnter={stateFunction}>
-        <div className="small-movie-card__image">
-          <img src={generatedFilm.previewImage} alt="Bohemian Rhapsody" width="280" height="175" />
-        </div>
-        <h3 className="small-movie-card__title">
-          <Link className="small-movie-card__link" to={`/films/1`}>{generatedFilm.name}</Link>
-        </h3>
+      <article
+        ref={cardRef}
+        className="small-movie-card catalog__movies-card"
+        onClick={trackActiveFilmId}
+      >
+        {isPreviewPlaying.status ? (
+          <SmallPlayer videoLink={generatedFilm.videoLink} isMuted={true}/>
+        ) : (
+          <React.Fragment>
+            <div className="small-movie-card__image">
+              <img src={generatedFilm.previewImage} alt="Bohemian Rhapsody" width="280" height="175"/>
+            </div>
+            <h3 className="small-movie-card__title">
+              <Link className="small-movie-card__link" to={`/films/1`}>{generatedFilm.name}</Link>
+            </h3>
+          </React.Fragment>
+        )}
       </article>
     </React.Fragment>
   );
@@ -22,7 +58,7 @@ const MovieCard = (props) => {
 
 MovieCard.propTypes = {
   generatedFilm: PropTypes.object.isRequired,
-  stateFunction: PropTypes.func.isRequired
+  trackActiveFilmId: PropTypes.func.isRequired
 };
 
 export default MovieCard;
